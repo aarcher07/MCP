7ee575feed5cf9608ee454ad00d9d430e85ed7e3
Andre Archer <andregregoryarcher@gmail.com>
2021-05-19 23:26:24 -0500

    changes to MCMC test

--- WholeCell/13_PDO_Pathway_Inference/MCMC_fixed_parameters/MCMC/MCMC.py
+++ WholeCell/13_PDO_Pathway_Inference/MCMC_fixed_parameters/MCMC/MCMC.py
@@ -2,8 +2,6 @@ import numpy as np
 from scipy.optimize import minimize
 from numpy.random import standard_normal,uniform,lognormal,exponential,gamma
 from scipy.linalg import sqrtm
-from scipy.special import lambertw
-import matplotlib.pyplot as plt
 
 
 def postdraws(logpost, rprior, initial_param, lbda = None,nsamp=2000):
@@ -156,15 +154,15 @@ def adaptive_postdraws(logpost, initial_param, nsamp=2000, beta=0.05, lbda = 0.1
 def maxpostdensity(rprior,logpost,max_opt_iters = 10, initial_param = None, 
 					maxiter = 10**3, jac=None, disp = True):
 	"""
-
-	@param rprior:
-	@param logpost:
-	@param max_opt_iters:
-	@param initial_param:
-	@param maxiter:
-	@param jac:
-	@param disp:
-	@return:
+	Computes the argmax of the posterior density with log probability, logpost
+	@param rprior: random draw from the prior
+	@param logpost: log of the posterior distribution
+	@param max_opt_iters: maximum iterations of the optimizer
+	@param initial_param: function that returns a starting point of the optimizer
+	@param maxiter: number of runs of the optimizers
+	@param jac: jacobian of the posterior density
+	@param disp: Boolean display optimizer output
+	@return: the argmax of the posterior density
 	"""
 
 	# set up optimization
@@ -202,94 +200,3 @@ def maxpostdensity(rprior,logpost,max_opt_iters = 10, initial_param = None,
 
 	return tmax
 
-def test_norm(m = 20):
-	"""
-	compares the MCMC walks given a target distribution covariance diag(range(1,m+1)) and a ones 
-	vector mean
-	"""
-
-	# generate covariances
-	cov_true = np.diag(range(1,m+1))
-	cov_inv_true = np.diag(1/np.array(range(1,m+1)))
-	mean_true = np.ones(m)
-
-	# likelihood and prior
-	loglik =lambda y: -0.5*np.dot(y-mean_true,np.dot(cov_inv_true,y-mean_true))
-	rprior =lambda n: standard_normal(n*m).reshape(n,m) + mean_true[np.newaxis,:]
-	initial_param = lambda: np.ones(m)
-	nsamples = 10**6
-
-	#fixed MCMC
-	tdraws = postdraws(rprior,loglik,initial_param, nsamp = nsamples)
-	for i in range(m):
-		n, bins, patches = plt.hist(x=tdraws[:,i], bins='auto', color='#0504aa',
-	                            alpha=0.7,density=True, rwidth=0.85)
-		mvn_obj = stats.multivariate_normal(mean=mean_true[i], cov=np.diag(cov_true)[i])
-		plt.plot(bins, mvn_obj.pdf(bins), color='black')
-		plt.show()
-
-	#adaptive MCMC
-	tdraws = adaptive_postdraws(loglik, initial_param, beta=0.05, lbda = 0.1, nsamp = nsamples)
-	for i in range(m):
-		n, bins, patches = plt.hist(x=tdraws[:,i], bins='auto', color='#0504aa',
-	                            alpha=0.7,density=True, rwidth=0.85)
-		mvn_obj = stats.multivariate_normal(mean=mean_true[i], cov=np.diag(cov_true)[i])
-		plt.plot(bins, mvn_obj.pdf(bins), color='black')
-		plt.show()
-
-
-def test():
-	"""
-	MCMC for Bayesian inference of falling object
-	"""
-
-	# create model
-	def hitf(vals):
-		g = vals[:,0]
-		m = vals[:,1]
-		r = vals[:,2]
-		coeff = vals[:,3]
-		h = vals[:,4]
-		a = (r**2 * np.pi * coeff)/m
-		term1 = h*a**2 /g+ 1
-		term2 = np.exp(-term1)
-		lamW = lambertw(-term2)
-		fterm = (lamW + term1)/a
-		return np.real(fterm)
-
-	# create prior, likelihood and posterior
-	def rprior(n):
-		return np.array([lognormal(2,0.25, size=n),
-						 exponential(1,n),
-						 gamma(5,20,n),
-						 uniform(0.1,2,n)]).T
-
-	logprior=lambda theta: stats.lognorm.logpdf(theta[0],s=0.25,scale=np.exp(2))+stats.expon.logpdf(theta[1],scale=1) +stats.gamma.logpdf(theta[2],a=5,scale=1/20) +stats.uniform.logpdf(theta[3],loc=0.1,scale=2-0.1)
-
-	h = np.array([5,10,20,30,80])
-	y = np.array([1.174, 1.576, 2.065, 2.715, 4.427])
-	f = lambda theta: hitf(np.concatenate((np.repeat(theta.reshape(1,-1),repeats=len(h),axis=0),h.reshape(-1,1)),1))
-
-	loglik =lambda theta: -0.5*np.dot(y-f(theta),y-f(theta)) / 0.1**2
-	logpost =lambda theta: loglik(theta) + logprior(theta)
-
-	# args for MCMC
-	nsamples = 10**3
-	tmax = maxpostdensity(rprior,logpost,disp=False)
-	initial_param = lambda: tmax
-
-	#fixed MCMC
-	tdraws = postdraws(rprior,logpost, lbda=0.1, nsamp = nsamples, initial_param = initial_param)
-	for i in range(tdraws.shape[1]):
-		plt.plot(range(int(nsamples)),tdraws[:,i])
-		plt.show()
-
-	#adaptive MCMC
-	tdraws = adaptive_postdraws(rprior,logpost, lbda = 0.1, nsamp = nsamples, initial_param = tmax)
-	for i in range(tdraws.shape[1]):
-		plt.plot(range(int(nsamples)),tdraws[:,i])
-		plt.show()
-
-if __name__ == '__main__':
-	test_norm()
-	test()
