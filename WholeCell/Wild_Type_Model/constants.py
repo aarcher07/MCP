597dcf078f67925f10876cf347b8384e6dd7388a
Andre Archer <andregregoryarcher@gmail.com>
2021-06-28 16:10:53 -0500

    constants

--- WholeCell/Wild_Type_Model/constants.py
+++ WholeCell/Wild_Type_Model/constants.py
@@ -10,121 +10,16 @@ import numpy as np
 
 HRS_TO_SECS = 60*60
 
-
-PARAMETER_LIST = ['kcatfDhaT','KmDhaTH', 'KmDhaTN',
-                  'kcatfDhaB', 'KmDhaBG', 
-                  'PermMCPPolar', 'PermMCPNonPolar',
-                  'PermCellGlycerol','PermCellPDO','PermCell3HPA',
-                  'dPacking', 
-                  'nmcps',
-                  'enz_ratio',
-                  'NADH_MCP_INIT','NAD_MCP_INIT']
-
-PERM_PARAMETER_LIST = ['PermMCPPolar', 'PermMCPNonPolar',
-                       'PermCellGlycerol','PermCellPDO','PermCell3HPA']
-
-                       
-VARIABLE_INIT_NAMES = ['G_MCP_INIT','H_MCP_INIT','P_MCP_INIT',
-                       'G_CYTO_INIT', 'H_CYTO_INIT','P_CYTO_INIT',
-                       'G_EXT_INIT', 'H_EXT_INIT','P_EXT_INIT']
+MODEL_PARAMETER_LIST = ['PermMCPPropanediol','PermMCPPropionaldehyde','PermMCPPropanol','PermMCPPropionyl','PermMCPPropionate',
+                        'nmcps',
+                        'PermCellPropanediol', 'PermCellPropionaldehyde', 'PermCellPropanol', 'PermCellPropionyl','PermCellPropionate'
+                        'VmaxfCDE', 'KmCDEPropanediol',
+                        'VmaxfPQf', 'KmPQfPropionaldehyde',
+                        'VmaxfPQr', 'KmPQfPropanol', 'KmPQfPropionyl']
 
 
-VARS_TO_TEX = {'kcatfDhaB': r'$k_{\text{cat}}^{f,\text{dhaB}}$',
-                'KmDhaBG': r'$K_{\text{M}}^{\text{Glycerol},\text{dhaB}}$',
-                'kcatfDhaT': r'$k_{\text{cat}}^{f,\text{dhaT}}$',
-                'KmDhaTH': r'$K_{\text{M}}^{\text{3-HPA},\text{dhaT}}$',
-                'KmDhaTN': r'$K_{\text{M}}^{\text{NADH},\text{dhaT}}$',
-                'NADH_MCP_INIT': r'$[\text{NADH}]$ ',
-                'NAD_MCP_INIT': r'$[\text{NAD+}]$ ',
-                'PermMCPPolar':r'$P_{\text{MCP},\text{Polar}}$',
-                'PermMCPNonPolar':r'$P_{\text{MCP},\text{Non-Polar}}$',
-                'PermCellGlycerol':r'$P_{\text{Cell},G}$',
-                'PermCellPDO':r'$P_{\text{Cell},P}$',
-                'PermCell3HPA':r'$P_{\text{Cell},H}$',
-                'dPacking': r'$\text{dPacking}$', 
-                'nmcps': r'$\text{Number of MCPs}$'}
 
-VARS_TO_UNITS = {'kcatfDhaB': '/s',
-                'KmDhaBG': 'mM',
-                'kcatfDhaT': '/s',
-                'KmDhaTH': 'mM',
-                'KmDhaTN': 'mM',
-                'NADH_MCP_INIT': 'mM',
-                'NAD_MCP_INIT': 'mM',
-                'PermMCPPolar':'m/s',
-                'PermMCPNonPolar':'m/s',
-                'PermCellGlycerol':'m/s',
-                'PermCellPDO':'m/s',
-                'PermCell3HPA':'m/s',
-                'dPacking': '', 
-                'nmcps': ''}
+VARIABLE_INIT_NAMES = ['PROPANEDIOL_MCP_INIT', 'PROPIONALDEHYDE_MCP_INIT', 'PROPANOL_MCP_INIT', 'PROPIONYL_MCP_INIT', 'PROPANOL_MCP_INIT',
+                       'PROPANEDIOL_CYTO_INIT', 'PROPIONALDEHYDE_CYTO_INIT', 'PROPANOL_CYTO_INIT', 'PROPIONYL_CYTO_INIT', 'PROPANOL_CYTO_INIT',
+                       'PROPANEDIOL_EXT_INIT', 'PROPIONALDEHYDE_EXT_INIT', 'PROPANOL_EXT_INIT', 'PROPIONYL_EXT_INIT', 'PROPANOL_EXT_INIT']
 
-PARAM_SENS_BOUNDS = {'kcatfDhaB': [400, 860], # /seconds Input
-                     'KmDhaBG': [0.6,1.1], # mM Input
-                     'kcatfDhaT': [40.,100.], # /seconds
-                     'KmDhaTH': [0.1,1.], # mM
-                     'KmDhaTN': [0.0116,0.48], # mM
-                     'NADH_MCP_INIT': [0.12,0.60],
-                     'PermMCPPolar': [10**-3, 10**-2],
-                     'PermMCPNonPolar': [5*10**-3, 10**-1],
-                     'PermCellGlycerol': [1e-8, 1e-2], 
-                     'PermCellPDO': [1e-6, 1e-2], 
-                     'PermCell3HPA': [1e-3, 1e-2],
-                     'dPacking': [0.3,0.64],
-                     'nmcps': [3.,30.]}
-
-
-PARAM_SENS_MIXED_BOUNDS = {'kcatfDhaB': [400, 860], # /seconds Input
-                            'KmDhaBG': [0.6,1.1], # mM Input
-                            'kcatfDhaT': [40.,100.], # /seconds
-                            'KmDhaTH': [0.1,1.], # mM
-                            'KmDhaTN': [0.0116,0.48], # mM
-                            'NADH_MCP_INIT': [0.12,0.60],
-                            'PermMCPPolar': np.log10([10**-4, 10**-2]),
-                            'PermMCPNonPolar': np.log10([10**-2, 10**-1]),
-                            'PermCellGlycerol': np.log10([1e-8, 1e-2]), 
-                            'PermCellPDO': np.log10([1e-6, 1e-2]), 
-                            'PermCell3HPA': np.log10([1e-3, 1e-2]),
-                            'dPacking': [0.3,0.64],
-                            'nmcps': [3.,30.]}
-
-PARAM_SENS_LOG2_BOUNDS = {'kcatfDhaB': np.log2([400, 860]), # /seconds Input
-                            'KmDhaBG': np.log2([0.6,1.1]), # mM Input
-                            'kcatfDhaT': np.log2([40.,100.]), # /seconds
-                            'KmDhaTH': np.log2([0.1,1.]), # mM
-                            'KmDhaTN': np.log2([0.0116,0.48]), # mM
-                            'NADH_MCP_INIT': np.log2([0.12,0.60]),
-                            'PermMCPPolar': np.log2([10**-4, 10**-2]),
-                            'PermMCPNonPolar': np.log2([10**-2, 10**-1]),
-                            'PermCellGlycerol': np.log2([1e-8, 1e-2]), 
-                            'PermCellPDO': np.log2([1e-6, 1e-2]), 
-                            'PermCell3HPA': np.log2([1e-3, 1e-2]),
-                            'dPacking': np.log2([0.3,0.64]),
-                            'nmcps': np.log2([3.,30.])
-                            }
-
-PARAM_SENS_LOG10_BOUNDS = {'kcatfDhaB': np.log10([400, 860]), # /seconds Input
-                            'KmDhaBG': np.log10([0.6,1.1]), # mM Input
-                            'kcatfDhaT': np.log10([40.,100.]), # /seconds
-                            'KmDhaTH': np.log10([0.1,1.]), # mM
-                            'KmDhaTN': np.log10([0.0116,0.48]), # mM
-                            'NADH_MCP_INIT': np.log10([0.12,0.60]),
-                            'PermMCPPolar': np.log10([10**-4, 10**-2]),
-                            'PermMCPNonPolar': np.log10([10**-2, 10**-1]),
-                            'PermCellGlycerol': np.log10([1e-8, 1e-2]), 
-                            'PermCellPDO': np.log10([1e-6, 1e-2]), 
-                            'PermCell3HPA': np.log10([1e-3, 1e-2]), 
-                            'dPacking': np.log10([0.3,0.64]),
-                            'nmcps': np.log10([3.,30.])}
-
-QOI_NAMES = ["maximum concentration of 3-HPA",
-             "Glycerol concentration after 5 hours",
-             "1,3-PDO concentration after 5 hours"]
-
-FUNCS_TO_NAMES = {'maximum concentration of 3-HPA': r'$\max_{t}\text{3-HPA}(t;\vec{p})$',
-                  'Glycerol concentration after 5 hours': r'$\text{Glycerol}(5\text{ hrs}; \vec{p})$',
-                  '1,3-PDO concentration after 5 hours': r'$\text{1,3-PDO}(5\text{ hrs}; \vec{p})$'}
-
-FUNCS_TO_FILENAMES = {'maximum concentration of 3-HPA': 'max3HPA',
-                     'Glycerol concentration after 5 hours': 'G5hrs',
-                     '1,3-PDO concentration after 5 hours': 'P5hrs'}
